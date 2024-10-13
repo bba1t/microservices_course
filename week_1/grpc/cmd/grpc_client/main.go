34606081aaf18df03a14a6df6fccfa223ca63b5e
Байтулла Омаров <bba1t@icloud.com>
2024-10-13 17:38:10 +0300

    удалил ненудные conspects

--- week_1/grpc/cmd/grpc_client/main.go
+++ week_1/grpc/cmd/grpc_client/main.go
@@ -9,6 +9,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
+	// пакет где лежит сгенерированный код
 	desc "github.com/olezhek28/microservices_course/week_1/grpc/pkg/note_v1"
 )
 
@@ -18,22 +19,32 @@ const (
 )
 
 func main() {
-	// insecure - не будет транспортного шифрования tls
+	// Этот вызов создает gRPC-клиента и открывает соединение с сервером по указанному адресу.
+	// grpc.Dial — это метод, который устанавливает соединение с gRPC-сервером.
+	// grpc.WithTransportCredentials(insecure.NewCredentials()) — указывает, что для подключения не используются безопасные соединения (без TLS/SSL).
+	// переменная conn будет хранить объект соединения
 	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect to server: %v", err)
 	}
-	defer conn.Close()
+	defer conn.Close() // закрывает соединение с gRPC-сервером
 
-	// desc - пакет где лежит сгенерированный код
-	c := desc.NewNoteV1Client(conn)
+	// Создается клиент для вызова RPC методов сервиса NoteV1, используя ранее установленное соединение conn
+	client := desc.NewNoteV1Client(conn)
 
-	// создаю контекст с таймаутом
+	// Создает контекст с тайм-аутом.
+	// context.Background() — это базовый контекст, который не содержит данных.
+	// time.Second — тайм-аут в 1 секунду. Это ограничение по времени для вызова RPC, чтобы он не выполнялся бесконечно.
+	// ctx — переменная, которая хранит контекст с тайм-аутом.
+	// cancel — функция для отмены этого контекста, которая должна быть вызвана для освобождения ресурсов, когда работа завершена.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	// через контекс вызываю метод
-	r, err := c.Get(ctx, &desc.GetRequest{Id: noteID})
+	// Вызов RPC метода Get на сервере через gRPC-клиента.
+	// client.Get — это вызов метода Get, который реализован на стороне сервера (получение заметки по ID).
+	// &desc.GetRequest{Id: noteID} — это аргумент запроса, который содержит структуру GetRequest с полем Id.
+	// r — будет содержать ответ от сервера (информацию о заметке), если запрос выполнится успешно.
+	r, err := client.Get(ctx, &desc.GetRequest{Id: noteID})
 	if err != nil {
 		log.Fatalf("failed to get note by id: %v", err)
 	}
--- week_1/grpc/cmd/grpc_server/main.go
+++ week_1/grpc/cmd/grpc_server/main.go
@@ -11,18 +11,20 @@ import (
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
-	// пакет где лежит сгенерированный код
+	// Пакет где лежит сгенерированный код
 	desc "github.com/olezhek28/microservices_course/week_1/grpc/pkg/note_v1"
 )
 
 const grpcPort = 50051
 
+// Нужно методами этой структуры соответствовать интерфейсу сгенерированному из proto файла в note_grpc.pb.go(89 строка)
 type server struct {
 	desc.UnimplementedNoteV1Server
 }
 
-// ручка Get ...
-// первый параметр всегда контекст, второй тот самый proto message
+// Ручка Get ... первый параметр всегда контекст, второй тот самый proto message
+// В сгенерированном файле node_grpc.pb.go уже есть все методы, реализованные как заглушки, но при имплементации, они перекрываются
+
 func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
 	// печатаю входящий proto message
 	log.Printf("Note id: %d", req.GetId())
@@ -44,16 +46,19 @@ func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetRespon
 }
 
 func main() {
-	// создаю сетевой сервер с протоколом и портом
+	// net.Listen("tcp", ...) — открывает сетевое соединение, которое будет "слушать" входящие запросы по протоколу TCP на указанном порту.
+	// fmt.Sprintf(":%d", grpcPort) — форматирует строку для указания порта в формате ":50051".
+	// lis — объект типа net.Listener, который будет слушать входящие соединения.
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
-	reflection.Register(s) // включаю возможность серверу рассказать о себе
+	s := grpc.NewServer()  // экземпляр gRPC сервера, который будет обрабатывать входящие от клиента gRPC-запросы
+	reflection.Register(s) // включаю возможность серверу рассказать какие методы у него есть (строка 223)
 
-	// метод из сгенерированного кода, второй аргумент - структура которая имплементировала api
+	// Когда gRPC-клиент отправляет запрос на сервис NoteV1, сервер s будет перенаправлять этот запрос к методам, реализованным в структуре server{}.
+	// Это нужно для того, чтобы gRPC-сервер знал, какую логику использовать для обработки запросов клиентов.
 	desc.RegisterNoteV1Server(s, &server{})
 
 	log.Printf("server listening at %v", lis.Addr())
